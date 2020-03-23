5089f1832cc9bfeb429f55d12fee723a9f7bbf1f
Héctor Reyes on HP <hector@dyasoftwaresolutions.onmicrosoft.com>
2020-03-23 13:52:01 -0600

    Operaciones basicas con 2 digitos

--- Calculadora/Calculadora/ViewModels/CalculadoraViewModel.cs
+++ Calculadora/Calculadora/ViewModels/CalculadoraViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using Xamarin.Forms;
 
 namespace Calculadora.ViewModels
@@ -9,11 +8,11 @@ namespace Calculadora.ViewModels
         private string numero;
         private string operacion;
         private string resultado;
-        private Double _numeroUno;
-        private Double _numeroDos;
+        private Double _numero;
         private Double _numeroTmp;
         public Command<object> OperationButtonCommand { get; set; }
         public Command<object> NumericButtonCommand { get; set; }
+        public Command<object> ResultButtonCommand { get; set; }
         public string Numero
         {
             get
@@ -55,47 +54,68 @@ namespace Calculadora.ViewModels
         {
             this.NumericButtonCommand = new Command<object>(this.NumericButton);
             this.OperationButtonCommand = new Command<object>(this.OperationButton);
+            this.ResultButtonCommand = new Command<object>(this.ResultButton);
             Numero = String.Empty;
             operacion = String.Empty;
         }
 
         private void NumericButton(object obj)
         {
-            Numero += obj.ToString();
+            if (obj.ToString() == "+/-")
+            {
+                _numero *= -1;
+                Numero = _numero.ToString();
+            }
+            else
+            {
+                Numero += obj.ToString();
+                Double.TryParse(Numero, out _numero);
+            }
+        }
+        private void OperationButton(object obj)
+        {
+            operacion = obj.ToString();
+            _numeroTmp = _numero;
+            Numero = String.Empty;
+            switch (operacion)
+            {
+                case "CE":
+                    _numeroTmp = 0;
+                    _numero = 0;
+                    Numero = String.Empty;
+                    Resultado = String.Empty;
+                    break;
 
-            bool _isNumber = Double.TryParse(Numero, out _numeroUno);           
-
+            }
+        }
+        private void ResultButton(object obj)
+        {
+            Double r = 0;
+            Double.TryParse(Numero, out _numero);
             switch (operacion)
             {
                 case "%":
                     break;
                 case "/":
-                    var r = _numeroTmp / _numeroUno;
-                    _numeroUno = r;
-                    Resultado = r.ToString();
-                    operacion = String.Empty;
+                    r = _numeroTmp / _numero;
                     break;
                 case "R":
+                    r = Math.Sqrt(_numeroTmp);
                     break;
                 case "X":
-                    break;
-                case "CE":
+                    r = _numeroTmp * _numero;
                     break;
                 case "-":
-                    break;
-                case "+/-":
+                    r = _numeroTmp - _numero;
                     break;
                 case "+":
-                    break;
-                case "=":
+                    r = _numeroTmp + _numero;
                     break;
             }
-        }
-        private void OperationButton(object obj)
-        {
-            operacion = obj.ToString();            
-            _numeroTmp = _numeroUno;
+            _numeroTmp = 0;
+            _numero = 0;
             Numero = String.Empty;
+            Resultado = r.ToString();
         }
     }
 }
